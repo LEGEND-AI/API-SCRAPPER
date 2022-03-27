7281772703717f9a08819ac9bb059c3d31f815e1
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-03-27 19:42:10 +0400

    Update translation.py

--- translation.py
+++ translation.py
@@ -5,7 +5,7 @@ my.telegram.org saytından APP-ID əldə etmək üçün Telegram Telefon Nömrə
 nümünə: `+99450*******`
 /start yenidən daxil etmək üçün istənilən mərhələdə başlayın""""
     AFTER_RECVD_CODE_TEXT = """I see!
-now please send the Telegram code that you received from Telegram!
+indi  zəhmət  olmasa Telegramdan aldığınız Telegram kodunu göndərin  ! 
 this code is only used for the purpose of getting the APP ID from my.telegram.org
 if you do not trust this bot dev, please host this bot yourself
 by opening https://github.com/LEGEND-AI/API-SCRAPPER and clicking on the Pink Button
