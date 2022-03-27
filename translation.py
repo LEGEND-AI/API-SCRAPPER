e5532f51804c963c6601ea0554f8f5ad3ab04240
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-03-28 00:45:22 +0400

    Update translation.py

--- translation.py
+++ translation.py
@@ -7,8 +7,8 @@ Enter your Telegram Phone Number, to get the APP-ID from my.telegram.org
     AFTER_RECVD_CODE_TEXT = """I see!
 now please send the Telegram code that you received from Telegram!
 this code is only used for the purpose of getting the APP ID from my.telegram.org
-if you do not trust this bot dev, please host this bot yourself
-by opening https://github.com/LEGEND-AI/API-SCRAPPER and clicking on the Pink Button
+
+
 
 /start at any stage to re-enter your details"""
 
