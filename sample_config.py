451e88138ac20f4c3d95afa50d24df281c76af2d
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-03-27 20:05:59 +0400

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -12,8 +12,8 @@ class Config:
     CHUNK_SIZE = 10280
     # MyTelegram.org
     # configurtion required while creating new application
-    APP_TITLE = os.environ.get("APP_TITLE", "Legend Api Scrapper Bot")
-    APP_SHORT_NAME = os.environ.get("APP_SHORT_NAME", " Legend Api Scrapper Bot")
+    APP_TITLE = os.environ.get("APP_TITLE", "Ledy Api Scrapper Bot")
+    APP_SHORT_NAME = os.environ.get("APP_SHORT_NAME", " Ledy Api Scrapper Bot")
     APP_URL = os.environ.get("APP_URL", "https://telegram.dog/LegendApiScrapperBot")
     # these platform informations were obtained
     # on 27.01.2020 21:15:50 IST
@@ -32,10 +32,10 @@ class Config:
     # https://github.com/SpEcHiDe/MyTelegramOrgRoBot/issues/3
     APP_DESCRIPTION = os.environ.get(
         "APP_DESCRIPTION",
-        "created by @LegendBoy_XD"
+        "Yaradıldı @Tenha055"
     )
     #
-    FOOTER_TEXT = os.environ.get("FTEXT", "❤️ @LegendBoy_XD")
+    FOOTER_TEXT = os.environ.get("FTEXT", "❤️ @Tenha055")
     # the strings used in the different messages
     # in the bot
     START_TEXT = os.environ.get("START_TEXT", Translation.START_TEXT)
