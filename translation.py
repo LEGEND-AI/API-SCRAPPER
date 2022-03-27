a51cca0043a05268c0234878795495863725ca98
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-03-27 19:37:28 +0400

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,6 +1,6 @@
 class Translation(object):
     START_TEXT = """Salam, Ledy APİ'ə xoş gəldin!
-
+Ledy APİ: Məni istifadə etdiyin üçün təşəkkürlər ❣️
 my.telegram.org saytından APP-ID əldə etmək üçün Telegram Telefon Nömrənizi daxil edin.
 nümünə: `+99450*******`
 /start yenidən daxil etmək üçün istənilən mərhələdə başlayın""""
