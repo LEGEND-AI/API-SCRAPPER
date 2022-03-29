acacdd18252934aea00cdd31125e345119b99346
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-03-30 00:18:23 +0400

    Update translation.py

--- translation.py
+++ translation.py
@@ -6,15 +6,13 @@ my.telegram.org saytından APP-ID əldə etmək üçün Telegram Telefon Nömrə
 /start yenidən başlamaq üçün basın"""
     AFTER_RECVD_CODE_TEXT = """yaxsı!
 __indi Telegramdan aldığınız Telegram kodunu göndərin!__
-this code is only used for the purpose of getting the APP ID from my.telegram.org
-if you do not trust this bot dev, please host this bot yourself
-by opening https://github.com/LEGEND-AI/API-SCRAPPER and clicking on the Pink Button
+**bu kod yalnız my.telegram.org saytından APP ID əldə etmək üçün istifadə olunur!**
 
-/start at any stage to re-enter your details"""
+/start yenidən başlatmaq üçün basın"""
 
     BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
 
-    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@LegendBoy_XD"
+    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@SOQrup"
 
     CANCELLED_MESG = "Bye! Please re /start the bot conversation"
 
