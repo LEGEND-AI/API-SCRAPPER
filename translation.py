33e5386c8081a36f7c044066ad36c3f09f99d7f3
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-03-30 00:23:50 +0400

    Update translation.py

--- translation.py
+++ translation.py
@@ -12,10 +12,10 @@ __indi Telegramdan aldığınız Telegram kodunu göndərin!__
 
     BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
 
-    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@SOQrup"
+    ERRED_PAGE = "bir şey səhvdir. failed to get app id. \n\n@SOQrup"
 
-    CANCELLED_MESG = "Bye! Please re /start the bot conversation"
+    CANCELLED_MESG = " /start yenidən başlatmaq..."
 
-    IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
+    IN_VALID_CODE_PVDED = "üzr istəyirik, daxil etdiyiniz kod düzgün deyil!"
 
-    IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number"
+    IN_VALID_PHNO_PVDED = "üzr istəyirik, daxil etdiyiniz nömr| düzgün deyil xaiş edirəm düzgün yazın!"
