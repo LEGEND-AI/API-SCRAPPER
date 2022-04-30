87c740ab08c9eb87f8f25df8f034f85fb27dd1f6
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-04-30 18:51:55 +0400

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,21 +1,21 @@
-class Translation(object):
-    START_TEXT = """Salam, Ledy AP Scrapper Bot'a xoş gəldin!
-my.telegram.org saytından APP-ID əldə etmək üçün Telegram Telefon Nömrənizi daxil edin.
+myass Translation(object):
+    START_TEXT = """👋 𝐒𝐚𝐥𝐚𝐦, 𝐋𝐞𝐝𝐲 𝐀𝐩𝐩 𝐒𝐜𝐫𝐚𝐩𝐩𝐞𝐫 𝐁𝐨𝐭'𝐚 𝐱𝐨𝐬 𝐠𝐞𝐥𝐝𝐢𝐧!
+✦ my.telegram.org saytından 𝐀𝐏𝐏-𝐈𝐃 əldə etmək üçün Telegram Telefon Nömrənizi daxil edin.
 
 
-/start yenidən başlamaq üçün basın"""
-    AFTER_RECVD_CODE_TEXT = """yaxsı!
-indi Telegramdan aldığınız Telegram kodunu göndərin!
-**bu kod yalnız my.telegram.org saytından APP ID əldə etmək üçün istifadə olunur!**
+✦ 𝗬𝗲𝗻𝗶𝗱𝗲𝗻 𝗯𝗮𝘀̧𝗹𝗮𝗺𝗮𝗾 𝘂̈𝗰̧𝘂̈𝗻 /start 𝗘𝗺𝗿𝗶𝗻𝗲 𝘁𝗼𝘅𝘂𝗻𝘂𝗻."""
+    AFTER_RECVD_CODE_TEXT = """✦𝙻𝙴𝙳𝚈 𝚂𝙴𝚁𝚅𝙸𝙲𝙴𝚂✦!
+indi Telegramdan aldığınız Telegram kodunu göndəririn
+❗Bu kod yalnız my.telegram.org saytından APP ID əldə etmək üçün istifadə olunur!
 
-/start yenidən başlatmaq üçün basın"""
+✦ 𝗬𝗲𝗻𝗶𝗱𝗲𝗻 𝗯𝗮𝘀̧𝗹𝗮𝗺𝗮𝗾 𝘂̈𝗰̧𝘂̈𝗻 /start 𝗘𝗺𝗿𝗶𝗻𝗲 𝘁𝗼𝘅𝘂𝗻𝘂𝗻."""
 
-    BEFORE_SUCC_LOGIN = "kod alındı. web səhifə silinir ..."
+    BEFORE_SUCC_LOGIN = k𝚘𝚍 𝙰𝚕ı𝚗𝚍ı.𝚠𝚎𝚋 𝚜𝚎𝚢𝚒𝚏𝚎 𝚜𝚒𝚕𝚒𝚗𝚒𝚛 ..."
 
-    ERRED_PAGE = "bir şey səhvdir. Sahiblə əlaqə saxlayın. \n\n@SOQrup"
+    ERRED_PAGE = "bir şey səhvdir, Xaiş edirik 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 𝐐𝐫𝐮𝐩𝐮𝐧𝐚 𝐲𝐚𝐳ı𝐧{}. \n\n@SOQrup"
 
-    CANCELLED_MESG = " /start yenidən başlatmaq..."
+    CANCELLED_MESG = "Yeniden başlatmaq üçün /start emrine toxunun..."
 
     IN_VALID_CODE_PVDED = "üzr istəyirik, daxil etdiyiniz kod düzgün deyil!"
 
-    IN_VALID_PHNO_PVDED = "üzr istəyirik, daxil etdiyiniz nömr| düzgün deyil xaiş edirəm düzgün yazın!"
+    IN_VALID_PHNO_PVDED = "üzr istəyirik, daxil etdiyiniz nömre düzgün deyil!"
